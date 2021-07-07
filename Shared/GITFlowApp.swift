2f1ef2513f1dcc40746d38451668cac3873e6fe8
Ani Poletich <poletichanna@mail.ru>
2021-07-08 00:20:14 +0300

    Initial Commit

---
+++ Shared/GITFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  GITFlowApp.swift
+//  Shared
+//
+//  Created by anna on 08.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct GITFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
